82834002cfbc6a2fe90602ca99ad2beba435280f
Tyler Bolty <tsbolty@gmail.com>
2020-05-28 12:04:14 -0600

    front end and back end servers working

---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  // export all models here
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+// var session = require("express-session");
+// var passport = require("./config/passport");
+const mongoose = require("mongoose");
+
+
+
+// const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const db = require("./models");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+// ???? client/build not public?
+app.use(routes);
+app.use(express.static("client/build"));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/DestinationGear");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
