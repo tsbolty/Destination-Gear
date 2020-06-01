9ca75519d35ea03c31747fb21a79a399e9bd929a
Tyler Bolty <tsbolty@gmail.com>
2020-06-01 13:28:09 -0600

    cleaned up code. fresh start

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -8,18 +8,6 @@ import ViewNotes from './ViewNotes'
 
 const Main = ()=>{
 
-    // const [loggedIn, setLoggedIn] = useState(false)
-    
-    // const handleAuthChange= ()=>{
-    //     const [loggedIn, setLoggedIn] = useState({
-    //         loggedIn: false
-    //     })
-    // }
-
-
-
-    // useEffect(() => {return Splash}, [])
-
     return(
         <>
             <DestinationCard />
